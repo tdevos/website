ba2bf14111d9762a86f76664e72ee77f1969a2fc
exh830 <thomas.devos.990@hr-rail.be>
2017-02-22 17:51:31 +0100

    Changed: Content, javascript and style

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-
+$(window).scroll(function() {
+    console.log('a');
+    $("nav.navbar").offset() &&
+    ($("nav.navbar").offset().top > 50 ? $("nav.navbar").addClass("top-nav-collapse") : $("nav.navbar").removeClass("top-nav-collapse"))
+});
