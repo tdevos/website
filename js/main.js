32aa2a1cbb3b02e0adef980a7b2fe36e0ca07f85
exh830 <thomas.devos.990@hr-rail.be>
2017-03-16 15:07:19 +0100

    Added: Particles JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $(window).scroll(function() {
-    console.log('a');
     $("nav.navbar").offset() &&
     ($("nav.navbar").offset().top > 50 ? $("nav.navbar").addClass("top-nav-collapse") : $("nav.navbar").removeClass("top-nav-collapse"))
+});
+
+particlesJS.load('particles-js', './js/particlesjs-config.json', function() {
+    console.log('callback - particles.js config loaded');
 });
